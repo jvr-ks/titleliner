e2ad9a2e457c6c74113fc1a4f63355ca5dc24646
Joerg von Roos <40567703+jvr-ks@users.noreply.github.com>
2023-05-21 19:55:01 +0200

    0.063

---
+++ updater_default_files$_$_$32.bat
@@ -0,0 +1,8 @@
+@rem updater_default_files$_$_$32.bat
+
+copy /Y "titleliner32.exe" "titleliner32.exe.default"
+
+copy /Y "titleliner.ini" "titleliner.ini.default"
+
+copy /Y "Licenses.txt" "Licenses.txt.default"
+
---
+++ updater_default_files$_$_$64.bat
@@ -0,0 +1,8 @@
+@rem updater_default_files$_$_$64.bat
+
+copy /Y "titleliner.exe" "titleliner.exe.default"
+
+copy /Y "titleliner.ini" "titleliner.ini.default"
+
+copy /Y "Licenses.txt" "Licenses.txt.default"
+
